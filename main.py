af133b35560e1ac4159e9ad747e94c69fe1aa907
12121985-star <studenttest430@gmail.com>
2025-07-10 10:50:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title("My Fisrt streamlit App")
 st.write("Hello! Creating a simple wen applications using Streamlit.")
+name=st.text_input("Enter you name:")
+if st.button("submit"):
+   st.write (f="Hello,{name}! Welocme to Streamlit.")
